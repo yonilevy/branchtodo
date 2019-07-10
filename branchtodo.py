35e0a8f07c0837f8c15038307e2d848f9ba2a309
yonilevy <yonilevy@gmail.com>
2019-07-10 19:52:19 +0300

    python3 + add some args

--- branchtodo.py
+++ branchtodo.py
@@ -1,12 +1,18 @@
 import unidiff
-import StringIO
+from io import BytesIO
 import subprocess
 from collections import defaultdict
 from clint.textui import colored
+import argparse
 
-def do():
-    raw_diffs = subprocess.check_output(["git", "diff", "origin/master", "--no-color", "-G", "TODO",  "-i"])
-    patch_set = unidiff.PatchSet(StringIO.StringIO(raw_diffs), encoding="utf8")
+
+def do(base_branch, include_dir, exclude_dir):
+    maybe_exclude_arg = [] if exclude_dir is None else\
+        [':(exclude){}'.format(exclude_dir)]
+    args = ["git", "diff", base_branch, "--no-color", "-G", "TODO", "-i",
+            '--', include_dir] + maybe_exclude_arg
+    raw_diffs = subprocess.check_output(args)
+    patch_set = unidiff.PatchSet(BytesIO(raw_diffs), encoding="utf8")
     path_info = defaultdict(list)
     for patch in patch_set:
         for hunk in patch:
@@ -17,16 +23,43 @@ def do():
     num_added = 0
     num_removed = 0
     for path in sorted(path_info.keys()):
-        print path
+        print(path)
         for line in path_info[path]:
             if line.is_added:
                 num_added += 1
             else:
-                num_removed +=1
-            val = "%s\t%s" % ((line.target_line_no if line.is_added else line.source_line_no), line.value.strip())
-            print colored.green("+:"+val ,bold=True) if line.is_added else colored.red("-:"+val, bold=True)
-        print
-    print "Total:", colored.green(str(num_added), bold=True), "additions and", colored.red(str(num_removed), bold=True), "removals"
+                num_removed += 1
+            val = "{}\t{}".format(
+                (line.target_line_no if line.is_added else line.source_line_no),
+                line.value.strip())
+            print(
+                colored.green("+:" + val, bold=True)
+                if line.is_added else
+                colored.red("-:" + val, bold=True))
+        print()
+    print("Total: {} additions and {} removals".format(
+        colored.green(str(num_added), bold=True),
+        colored.red(str(num_removed), bold=True)))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Review TODOs in current git branch.')
+    parser.add_argument('dir', type=str, nargs='?', help='Directory to include', default='.')
+    parser.add_argument('--exclude-dir',
+                        '-x',
+                        type=str,
+                        default=None,
+                        help='Directory to exclude')
+    parser.add_argument('--base-branch',
+                        '-b',
+                        type=str,
+                        dest='base',
+                        default='origin/master',
+                        help='base branch to search TODOs against (default: origin/master)')
+
+    args = parser.parse_args()
+    do(base_branch=args.base, include_dir=args.dir, exclude_dir=args.exclude_dir)
+
 
 if __name__ == "__main__":
-    do()
+    main()
